2533198be579ac4c966c89a7b9b0db08f84f25a0
Muhammad Umar <muhammadumar@Muhammads-MacBook-Pro.local>
2022-12-23 08:12:00 +0500

    add query

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require('express');
-const {graphqlHTTP} = require('express-graphql')
+const {graphqlHTTP} = require('express-graphql');
+const {buildSchema} = require('graphql');
+
 const app = express();
 
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String,
+    test: String
+  },
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+    hello: () => {
+        return 'Hello world!';
+    },
+    test: () => {
+        return 'testing!!!';
+    },
+};
+
 app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
     graphiql: true,
 }));
 
